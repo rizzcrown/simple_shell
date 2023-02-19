27af5456df0a4073f864176c7d7d72ba03b565e3
rizzcrown <jasonoyato@yahoo.com>
2023-02-19 09:56:18 -0800

    whatever betty wants she gets

--- simple_shell.c
+++ simple_shell.c
@@ -6,31 +6,47 @@
 
 #define MAX_COMMAND_LENGTH 1000
 
+/**
+ * main - Entry point for simple shell program
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ * @envp: Array of environmental variable strings
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[], char **envp)
 {
-    char command[MAX_COMMAND_LENGTH], *args[MAX_COMMAND_LENGTH], *arg;
-    pid_t pid;
-    int status, i;
-    while (printf("simple-shell->> "), fgets(command, MAX_COMMAND_LENGTH, stdin) != NULL)
-    {
-        if (command[0] == '\n')
-            continue;
-        for (arg = strtok(command, " \t\n"), i = 0; arg != NULL; arg = strtok(NULL, " \t\n"))
-            args[i++] = arg;
-        args[i] = NULL;
-        if ((pid = fork()) == 0)
-        {
-            execvp(args[0], args);
-            fprintf(stderr, "Simple_shell: Command not found: %s\n", args[0]);
-            exit(1);
-        }
-        else if (pid < 0)
-        {
-            fprintf(stderr, "Simple shell: fork failed\n");
-            exit(1);
-        }
-        else
-            waitpid(pid, &status, 0);
-    }
-    return 0;
+	char command[MAX_COMMAND_LENGTH], *args[MAX_COMMAND_LENGTH], *arg;
+	pid_t pid;
+	int status, i;
+
+	while (1)
+	{
+		printf("simple-shell->> ");
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+			break;
+		if (command[0] == '\n')
+			continue;
+		for (arg = strtok(command, " \t\n"), i = 0;
+		     arg != NULL;
+		     arg = strtok(NULL, " \t\n"))
+			args[i++] = arg;
+			args[i] = NULL;
+		pid = fork();
+		if (pid == 0)
+		{
+			execvp(args[0], args);
+			fprintf(stderr, "Simple_shell: Command not found: %s\n", args[0]);
+			exit(1);
+		}
+		else if (pid < 0)
+		{
+			fprintf(stderr, "Simple shell: fork failed\n");
+			exit(1);
+		}
+		else
+			waitpid(pid, &status, 0);
+	}
+	return (0);
 }
+
